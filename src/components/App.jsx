56dc5e4aee8f906445548af75a9d2294eb85fb35
luisf756 <luchof756@gmail.com>
2023-06-06 18:41:20 -0500

    add component and config initial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,20 @@
+const section = {
+  width: '100vw',
+  height: '40vh',
+  margin: ' 0 auto',
+  marginBottom: '100px',
+  paddingRight: '100px',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+};
+
+state = {
+  contacts: [],
+  name: '',
+};
+
 export const App = () => {
   return (
     <div
@@ -7,10 +24,17 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
     </div>
   );
 };
