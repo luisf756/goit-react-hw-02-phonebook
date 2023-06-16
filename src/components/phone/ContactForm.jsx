25a53df053b7411efe1fbc72d6b7829fcfb54d53
luisf756 <luchof756@gmail.com>
2023-06-15 20:09:37 -0500

    add pattern

--- src/components/phone/ContactForm.jsx
+++ src/components/phone/ContactForm.jsx
@@ -33,7 +33,7 @@ export default class ContactForm extends Component {
           <input
                 type="tel"
                 name="number"
-                //  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                 title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                 required
             />
